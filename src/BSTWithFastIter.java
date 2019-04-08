e9050dfaefd9a0eba97e7de31bbbdf23e2aa31b3
jerred <jerred@lamproslabs.com>
2019-04-08 15:19:29 -0400

    first commit

---
+++ src/BSTWithFastIter.java
@@ -0,0 +1,60 @@
+import java.util.NoSuchElementException;
+
+public class BSTWithFastIter<E extends Comparable<E>> extends BST {
+    private java.util.Stack<TreeNode<E>> stack = new java.util.Stack<>();
+    private TreeNode<E> current = root;
+    private E lastReturned = null;
+
+    public BSTWithFastIter() {
+
+    }
+    public BSTWithFastIter(E[] objects) {
+        for (int i = 0; i < objects.length; i++)
+            add(objects[i]);
+    }
+    public java.util.Iterator<E> iterator() {
+        return new Iterator();
+    }
+
+    private class Iterator implements java.util.Iterator<E> {
+            public Iterator(){
+
+            }
+        public boolean hasNext() {
+            if (current != null && !stack.empty())
+                return true;
+            else
+                return false;
+
+        }
+
+        public E next() {
+            if (hasNext() == false) {
+                throw new NoSuchElementException();
+            }
+
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+                TreeNode<E> node = stack.pop();
+                lastReturned = node.element;
+                current = node.right;
+
+
+            }
+            return lastReturned;
+
+
+        }
+
+        public void remove() {
+            if (lastReturned == null)
+                throw new IllegalStateException();
+            else {
+                delete(lastReturned);
+                lastReturned = null;
+            }
+
+        }
+    }
+}
