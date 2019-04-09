0d2bc4e98dd894127be0e9aedc51d4e8456c6760
jerred <jerred@lamproslabs.com>
2019-04-08 21:40:47 -0400

    this adds

--- src/BSTWithFastIter.java
+++ src/BSTWithFastIter.java
@@ -1,61 +1,67 @@
 import java.util.NoSuchElementException;
 
 public class BSTWithFastIter<E extends Comparable<E>> extends BST {
-    private java.util.Stack<TreeNode<E>> stack = new java.util.Stack<>();
-    private TreeNode<E> current = root;
-    private E lastReturned = null;
 
     public BSTWithFastIter() {
-        super();
     }
+
     public BSTWithFastIter(E[] objects) {
         for (int i = 0; i < objects.length; i++)
-            add( objects[i]);
+            add(objects[i]);
     }
 
+    @Override
     public java.util.Iterator<E> iterator() {
-        return new Iterator();
+        return new InorderIterator();
     }
 
-    private class Iterator implements java.util.Iterator<E> {
-            public Iterator(){
+    private class InorderIterator implements java.util.Iterator<E> {
 
-            }
-        public boolean hasNext() {
-            if (current != null || stack.empty())
-                return true;
-            else
-                return false;
+        private java.util.Stack<TreeNode<E>> stack = new java.util.Stack<>();
+        private TreeNode<E> current = root;
+        private E lastReturned = null;
 
-        }
-
-        public E next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-
-            while (current != null) {
+        public InorderIterator(){
+            while(current != null){
                 stack.push(current);
                 current = current.left;
-                TreeNode<E> node = stack.pop();
-                lastReturned = node.element;
-                current = node.right;
-
-
             }
-            return lastReturned;
-
-
         }
 
-        public void remove() {
-            if (lastReturned == null)
-                throw new IllegalStateException();
-            else {
-                delete(lastReturned);
-                lastReturned = null;
+        @Override
+        public boolean hasNext(){
+            if(current != null || !stack.isEmpty()) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        @Override
+        public E next(){
+            if(!hasNext()){
+                throw new NoSuchElementException();
+            }
+            while(current != null){
+                stack.push(current);
+                current = current.left;
             }
 
+            current = stack.pop();
+            TreeNode<E> temp = current;
+            lastReturned = temp.element;
+            current = current.right;
+            return lastReturned;
+        }
+
+        @Override
+        public void remove(){
+            if(lastReturned == null){
+                throw new IllegalStateException();
+            }
+            delete(lastReturned);
+            lastReturned = null;
         }
     }
 }
